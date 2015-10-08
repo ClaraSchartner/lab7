555e1e578ab74edccc6bbfc0fe4c610b17b3b863
unknown <Clara.Schartner@gmail.com>
2015-10-08 11:32:22 +0200

    beginning least squares, careful, might have a lot of mistakes!

--- ridge.R
+++ ridge.R
@@ -1,10 +1,11 @@
 
-ridgereg <- function(formula, data){
+ridgereg <- function(formula, data, lambda=0.1){
   X <- model.matrix(formula, data)
   y <- as.matrix(data[all.vars(formula)[!(all.vars(formula) %in% colnames(X))]])
   stopifnot(is.numeric(y)&is.numeric(X))
-  xnorm<-as.matrix(data-apply(data,2,mean))%*%as.matrix(var(data)) #do you think this is right?
-  
-  
+  xnorm<-as.matrix(t(t(X)-apply(X,2,mean)))#/diag(var(X)) #how do you think we have to devide my variance?
+I<-matrix(0,nrow=ncol(X),ncol=ncol(X))
+diag(I)<-1
+  ans<-solve(t(xnorm)%*%xnorm+lambda*I)%*%(t(xnorm)%*%y)
   
 }
