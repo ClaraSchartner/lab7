c89d90ec1b188cc0a63a836a28b530fea0feffd3
Araya Eamrurksiri <araea382@student.liu.se>
2015-10-11 09:45:30 +0200

    unit test for ridgereg

--- lab7/tests/testthat/tests-ridgereg.R
+++ lab7/tests/testthat/tests-ridgereg.R
@@ -1,12 +1,13 @@
 context("ridgereg")
 
-#not finished yet
-library(MASS)
-data("faithful")
-x <- lm.ridge(eruptions~waiting, data=faithful)
 y <- ridgereg(eruptions~waiting, data=faithful)
     
-testthat("equal",{
-    expect_equal(x,y)
+test_that("coefficients",{
+    expect_less_than(y$coefficients[1] - 0, 0.001)
+    expect_less_than(y$coefficients[2] - 0.0756278, 0.001)
+})
+
+test_that("class",{
+    expect_is(y$coefficients,"numeric")
 })
 
