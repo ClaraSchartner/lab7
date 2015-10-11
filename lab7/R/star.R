48200b313f69f2549521bd8171cf0c4edeb7eb7a
unknown <Clara.Schartner@gmail.com>
2015-10-11 21:11:32 +0200

    start dividing data

--- lab7/R/star.R
+++ lab7/R/star.R
@@ -7,8 +7,16 @@ data("flights")
 
 flights$dep_time<-round(flights$dep_time/100,0) #necesary for matching
 
-#rel<-select(flights, dep_time, dep_delay, distance, air_time, arr_delay)
 data("weather")
-#weather
+
 rel.we<-left_join(flights,weather,by=c("origin","year","month", c("dep_time"="hour")))
 rel<-select(rel.we, dep_time, dep_delay, distance, air_time, arr_delay, temp, dewp, humid, wind_speed, pressure)
+rel %>% filter(complete.cases(.))
+
+ab<-filter(rel,complete.cases(rel))
+library(caret)
+ind<-createDataPartition(ab$arr_delay, p=c(0.8), list=FALSE)
+temp<-dim(ab)[1]
+temp1<-c(1:temp) %in%  ind  #checking if index in to get TRUE FALSE and put them into filter #optimize
+training<-filter(ab, temp1)
+valtest<-filter(ab, !temp1)
