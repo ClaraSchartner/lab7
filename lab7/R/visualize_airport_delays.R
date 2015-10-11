8f65d9ead3ff471be48f5c088a1d1ed2a017e42a
Araya Eamrurksiri <araea382@student.liu.se>
2015-10-11 10:05:54 +0200

    move library inside function

--- lab7/R/visualize_airport_delays.R
+++ lab7/R/visualize_airport_delays.R
@@ -1,11 +1,11 @@
-library(dplyr)
-library(tidyr)
-library(ggplot2)
-library(nycflights13)
-
 visualize_airport_delays <- function(){
+    library(dplyr)
+    library(tidyr)
+    library(ggplot2)
+    library(nycflights13)
     data("flights")
     data("airports")
+    
     subset_f <- select(flights, flight, origin, dest, dep_delay, arr_delay)
     subset_a <- select(airports, faa, name, lat, lon)
     new_flights <- mutate(subset_f, total_delay = dep_delay + arr_delay)
