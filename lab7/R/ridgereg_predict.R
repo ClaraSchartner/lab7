62cf6ebce4d2750e6647180d984e7132e549b615
Araya Eamrurksiri <araea382@student.liu.se>
2015-10-16 16:16:06 +0200

    predict

--- lab7/R/ridgereg_predict.R
+++ lab7/R/ridgereg_predict.R
@@ -8,18 +8,13 @@
 #'@return produce a vector of predictions.
 #'
 
-predict.ridgereg <- function(x, ...){
-    return(x$fitted)
-}
-it should predict..
 predict.ridgereg <- function(x, x_values = "default"){
-  
   if(identical(x_values, "default")){
     # predict the same data the model was created on:
     return(x$fitted)
   }else{
     xnorm<-as.matrix(x)
-    X<-as.matrix(x)
+    X<-as.matrix(cbind(1,x_values))
     y<-as.numeric(y)
     I <- matrix(0,nrow=ncol(X),ncol=ncol(X))
     diag(I) <- 1
@@ -28,7 +23,7 @@ predict.ridgereg <- function(x, x_values = "default"){
     beta <- as.vector(ans)
     names(beta) <- colnames(X)
     #the fitted values 
-    fit <- X %*% x$beta ##include in ridge!!
+    fit <- X %*% beta ##include in ridge!!
     fit <- as.vector(fit)
     return(fit)
   }
